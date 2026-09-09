19b37e1ef7b6a73c9131931f6c8a20df54833b1d
tomasz <csharp@406-00>
2026-09-09 08:47:58 +0200

    Add project files.

---
+++ P03WeatherForecastWPF.Client/Models/Weather.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03WeatherForecastWPF.Client.Models
+{
+    internal class Weather
+    {
+        public string Time { get; set; }
+        public double Temperature2m { get; set; }
+    }
+}
---
+++ P03WeatherForecastWPF.Client/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using P03WeatherForecastWPF.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03WeatherForecastWPF.Client.ViewModels
+{
+    internal class MainViewModel
+    {
+        private string _cityName = "Warszawa";
+        private City[] _cities;
+        private City _selectedCity;
+        private Weather weather;
+
+
+
+        public string CityName
+        {
+            get { return _cityName; }
+            set { _cityName = value; }
+        }
+
+        public City[] Cities
+        {
+            get { return _cities; }
+            set { _cities = value; }
+        }
+
+        public City SelectedCity
+        {
+            get { return _selectedCity; }
+            set { _selectedCity = value; }
+        }
+
+        public Weather Weather
+        {
+            get { return weather; }
+            set { weather = value; }
+        }
+    }
+}
