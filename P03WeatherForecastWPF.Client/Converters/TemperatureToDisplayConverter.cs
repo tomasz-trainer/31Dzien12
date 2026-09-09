bb0a49ea21208f8340f9b55b52f331d654d1261b
tomasz <csharp@406-00>
2026-09-09 11:35:15 +0200

    q

---
+++ P03WeatherForecastWPF.Client/Converters/TemperatureToDisplayConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace P03WeatherForecastWPF.Client.Converters
+{
+    internal class TemperatureToDisplayConverter : IValueConverter
+    {
+        private const string _tempCode = "°C";
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
